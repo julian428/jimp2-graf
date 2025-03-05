ea553729cbaebfbf5caca48e1b5229c13af73918
Julian <julianjometzger@gmail.com>
2025-03-05 17:53:29 +0100

    feat: Program generuje macierz sąsiedztwa o rozmiarze podanym przez użytkownika. Narazie tylko losową.

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
 int main(int argc, char** argv){
 	int vertex = atoi(argv[1]);
 	char* polecenie = argv[2];
+
+	int* neighbours = (int*)malloc(vertex*vertex*sizeof(int));
+
+	for(int i = 0; i < vertex; i++){
+		for(int j = 0; j < vertex; j++){
+			neighbours[i * vertex + j] = round((double)rand()/RAND_MAX);
+		}
+	}
+
+
+	for(int i = 0; i < vertex; i++){
+		for(int j = 0; j < vertex; j++){
+			printf("%d", neighbours[i * vertex + j]);
+		}
+		printf("\n");
+	}
+	free(neighbours);
 }
