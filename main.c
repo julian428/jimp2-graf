a39ca232081b8c4941931f8e62fab4fcc41b0330
Julian <julianjometzger@gmail.com>
2025-03-07 07:07:10 +0100

    feat: Added a function to get the current date and time.

--- main.c
+++ main.c
@@ -2,10 +2,22 @@
 #include<stdio.h>
 #include<math.h>
 #include<time.h>
+#include<string.h>
+
+char* gettime(){
+	time_t t;
+	time(&t);
+	struct tm *tm_info = localtime(&t);
+
+	char* dateBuffer = (char*)malloc(20 * sizeof(char));
+	strftime(dateBuffer, 20, "%d.%m.%Y@%H:%M",tm_info);
+
+	return dateBuffer;
+}
 
 int main(int argc, char** argv){
 	int vertex = atoi(argv[1]);
-	char* polecenie = argv[2];
+	char* task = argv[2];
 	srand(time(NULL));
 
 	int* neighbours = (int*)malloc(vertex*vertex*sizeof(int));
@@ -23,5 +35,10 @@ int main(int argc, char** argv){
 		}
 		printf("\n");
 	}
+
+	char fileName[30] = "graph-";
+	strcat(fileName, gettime());
+	printf("%s", fileName);
+
 	free(neighbours);
 }
